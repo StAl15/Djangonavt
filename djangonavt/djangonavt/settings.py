359b05acd6f1be81e0087fc32a26569d91ffca9a
Alex <stihinaleksander@yandex.ru>
2021-12-30 23:53:56 +0500

    upd

--- djangonavt/djangonavt/settings.py
+++ djangonavt/djangonavt/settings.py
@@ -8,6 +8,8 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
+
+!!! серверная часть !!!
 """
 
 from pathlib import Path
