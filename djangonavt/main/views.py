632f0f0fecf2b68daab7156668edf49f64e75519
Alex <stihinaleksander@yandex.ru>
2022-01-03 00:00:09 +0500

    upd backend

--- djangonavt/main/views.py
+++ djangonavt/main/views.py
@@ -1,5 +1,23 @@
-from django.shortcuts import render
+import logging
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 
+
 def index(request):
-    return render(request, 'main/index.html')
+    data = {
+        'title' : 'Ural Statelite',
+        'temperature': 1150,
+        'quantity_days': 520,
+        'speed': 50
+    }
+    return render(request, 'main/index.html', data) #передаем значения
+
+def start(request):
+    print("fuck")
+    data = {
+        'title' : 'Ural Statelite',
+        'temperature': 11500,
+        'quantity_days': 5200,
+        'speed': 500
+    }
+    return render(request, 'main/index.html', data)
