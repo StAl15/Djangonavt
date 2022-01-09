ea05fa85c4cabf9372cdbb8bd4cb7b3230b94d4c
Alex <stihinaleksander@yandex.ru>
2022-01-09 10:36:57 +0500

    upd views

--- djangonavt/main/views.py
+++ djangonavt/main/views.py
@@ -8,7 +8,12 @@ def index(request):
         'title' : 'Ural Statelite',
         'temperature': 1150,
         'quantity_days': 520,
-        'speed': 50
+        'speed': 50,
+        'amperage_k': 10,
+        'amperage_n': 10,
+        'pressure': 16,
+        'voltage': 20,
+        'traction': 10
     }
     return render(request, 'main/index.html', data) #передаем значения
 
